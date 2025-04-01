c52a34da50065d0325e080d78a4ff62b018b38ed
Mensun <mensun@bu.edu>
2025-04-01 17:52:24 -0400

    forgot to revert API error testing change

--- src/app/api/events/route.ts
+++ src/app/api/events/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
   try {
-    const response = await fetch(`http://localhost:8000/eves`, {
+    const response = await fetch(`http://localhost:8000/events`, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
